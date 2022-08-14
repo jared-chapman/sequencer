716f9fde99ac46534255807b798123852632fde0
Jared Chapman <jared.chapman.0612@gmail.com>
2022-08-14 10:28:02 -0500

    Delete Adafruit_NeoTrellis_M4_Library directory

--- Adafruit_NeoTrellis_M4_Library/examples/Filter_FIR_trellis/filters.h
+++
@@ -1,4 +0,0 @@
-// Number of coefficients
-#define NUM_COEFFS 100
-extern short low_pass[];
-extern short band_pass[];
--- Adafruit_NeoTrellis_M4_Library/examples/sixteen_step_sequencer/AudioSampleCp02.h
+++
@@ -1,3 +0,0 @@
-// Audio data converted from WAV file by wav2sketch
-
-extern const unsigned int AudioSampleCp02[6401];
